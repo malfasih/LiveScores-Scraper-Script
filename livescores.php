08027aa346dab9bdb34f42bb5221d3bd09b7aec3
malfasih <malfasih@icloud.com>
2018-08-10 19:28:51 +0000

    Update livescores.php

--- livescores.php
+++ livescores.php
@@ -5,5 +5,19 @@ $output = ob_get_clean();
 
 $output = json_decode($output);
 
-print_r($output);
+$matches = array();
+
+foreach($output as $match)
+{
+	$scores = explode('-', str_replace(" ", "", $match->result));
+
+	$time = str_replace(" ", "", trim($match->hours));
+
+	$home_score = trim($scores[0]);
+	$away_score = trim($scores[1]);
+
+	$matches[] = array('time' => $time, 'home' => trim($match->team1), 'away' => trim($match->team2), 'home_score' => $home_score, 'away_score' => $away_score);
+}
+
+print_r($matches);
 ?>
